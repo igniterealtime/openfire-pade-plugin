56aec1751a03211282e5edcffd606becc86c15df
Dele Olajide <dele@4ng.net>
2020-04-13 18:53:33 +0100

    set baseline

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 call mvn clean package
 
-rem copy ofmeet\target\ofmeet.jar C:\openfire_4_5_1\plugins\ofmeet.jar
+copy ofmeet\target\ofmeet.jar C:\openfire_4_5_1\plugins\ofmeet.jar
 copy offocus\target\offocus.jar C:\openfire_4_5_1\plugins\offocus.jar
-rem copy pade\target\pade.jar C:\openfire_4_5_1\plugins\pade.jar
+copy pade\target\pade.jar C:\openfire_4_5_1\plugins\pade.jar
 
 pause

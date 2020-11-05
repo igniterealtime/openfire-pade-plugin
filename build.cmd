91c49d5fca4433995333a804ccf3bf5a30abecf2
Dele Olajide <dele@4ng.net>
2020-11-05 14:55:33 +0000

    prepare for ver 1.0.3

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
-call mvn clean package
+call mvn clean package -Dmaven.test.skip=true
 
-copy ofmeet\target\ofmeet.jar C:\openfire_4_5_4\plugins\ofmeet.jar
-copy offocus\target\offocus.jar C:\openfire_4_5_4\plugins\offocus.jar
-copy pade\target\pade.jar C:\openfire_4_5_4\plugins\pade.jar
+copy ofmeet\target\ofmeet.jar C:\openfire_4_6_0\plugins\ofmeet.jar
+copy offocus\target\offocus.jar C:\openfire_4_6_0\plugins\offocus.jar
+copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
 
 pause
--- rebuild.cmd
+++ rebuild.cmd
@@ -1,7 +1,7 @@
-call mvn package
+call mvn package -Dmaven.test.skip=true
 
-copy ofmeet\target\ofmeet.jar C:\openfire_4_5_3\plugins\ofmeet.jar
-copy offocus\target\offocus.jar C:\openfire_4_5_3\plugins\offocus.jar
-copy pade\target\pade.jar C:\openfire_4_5_3\plugins\pade.jar
+copy ofmeet\target\ofmeet.jar C:\openfire_4_6_0\plugins\ofmeet.jar
+copy offocus\target\offocus.jar C:\openfire_4_6_0\plugins\offocus.jar
+copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
 
 pause

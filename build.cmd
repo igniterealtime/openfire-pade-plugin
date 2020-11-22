07f2f95600df700209d85f905fc6b69d28cea1ba
Dele Olajide <dele@4ng.net>
2020-11-22 00:24:03 +0000

    first pass at JVB2

--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@ call mvn clean package -Dmaven.test.skip=true
 
 copy ofmeet\target\ofmeet.jar C:\openfire_4_6_0\plugins\ofmeet.jar
 copy offocus\target\offocus.jar C:\openfire_4_6_0\plugins\offocus.jar
-copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
+rem copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
 
 pause

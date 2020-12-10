20825e3b934f9b5aae0ff5a84601a2f2e6f76ce3
Dele Olajide <dele@4ng.net>
2020-12-10 18:15:38 +0000

    version 1.2.0

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 call mvn clean package -Dmaven.test.skip=true
 
 copy ofmeet\target\ofmeet.jar C:\openfire_4_6_0\plugins\ofmeet.jar
-rem copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
+copy pade\target\pade.jar C:\openfire_4_6_0\plugins\pade.jar
 
 pause

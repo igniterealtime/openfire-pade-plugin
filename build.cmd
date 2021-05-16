cf4eda4a658be67a82e560a7b09df111c7252b7f
Dele Olajide <dele@4ng.net>
2021-05-16 21:19:28 +0100

    fix typo

--- build.cmd
+++ build.cmd
@@ -2,8 +2,8 @@ call mvn clean package -Dmaven.test.skip=true
 
 cd target
 rename pade-openfire-plugin-assembly.jar pade.jar
-rd "C:\openfire_4_6_3\plugins\acs" /q /s
-del "C:\openfire_4_6_3\plugins\acs.jar" 
+rd "C:\openfire_4_6_3\plugins\pade" /q /s
+del "C:\openfire_4_6_3\plugins\pade.jar" 
 del "C:\openfire_4_6_3\logs\*.*"
 copy pade.jar C:\openfire_4_6_3\plugins\pade.jar
 

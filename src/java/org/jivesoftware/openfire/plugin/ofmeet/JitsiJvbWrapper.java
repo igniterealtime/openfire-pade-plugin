f7c5eb0b64837542b4abc6c8c3fa8aafc8e0f59e
naive924 <sattontanabe@gmail.com>
2025-05-17 11:47:21 +0900

    feat: Updated the latest jitsi

--- src/java/org/jivesoftware/openfire/plugin/ofmeet/JitsiJvbWrapper.java
+++ src/java/org/jivesoftware/openfire/plugin/ofmeet/JitsiJvbWrapper.java
@@ -219,7 +219,7 @@ public class JitsiJvbWrapper implements ProcessListener
 
         String customOptions = JiveGlobals.getProperty( "org.jitsi.videobridge.ofmeet.jvb.jvm.customOptions", defaultOptions);
         if(!customOptions.isEmpty() && !customOptions.endsWith(" ")) customOptions += " ";
-        final String cmdLine = javaExec + " " + customOptions + "-Dconfig.file=" + configFile + " -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=" + jvbHomePath + " -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config -Djava.util.logging.config.file=./logging.properties -Djdk.tls.ephemeralDHKeySize=2048 -cp ./jitsi-videobridge-2.1-SNAPSHOT.jar" + File.pathSeparator + "./jitsi-videobridge-2.1-SNAPSHOT-jar-with-dependencies.jar org.jitsi.videobridge.MainKt  --apis=rest";
+        final String cmdLine = javaExec + " " + customOptions + "-Dconfig.file=" + configFile + " -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=" + jvbHomePath + " -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config -Djava.util.logging.config.file=./logging.properties -Djdk.tls.ephemeralDHKeySize=2048 -cp ./jitsi-videobridge-2.3-SNAPSHOT.jar" + File.pathSeparator + "./jitsi-videobridge-2.3-SNAPSHOT-jar-with-dependencies.jar org.jitsi.videobridge.MainKt  --apis=rest";
 
         if ( JiveGlobals.getBooleanProperty("ofmeet.use.internal.jvb", true) )
         {

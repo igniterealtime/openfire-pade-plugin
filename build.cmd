e03171bb72f9624afc71d831189dbf04801e0fae
Dele Olajide <dele@4ng.net>
2026-09-08 12:48:44 +0100

    build 1248

--- build.cmd
+++ build.cmd
@@ -2,29 +2,5 @@ call mvn clean package -Dmaven.test.skip=true
 
 cd target
 rename pade-openfire-plugin-assembly.jar pade.jar
-rd "D:\Openfire\openfire_snapshot\plugins\pade" /q /s
-del "D:\Openfire\openfire_snapshot\plugins\pade.jar" 
-del /q "D:\Openfire\openfire_snapshot\logs\*.*"
-copy pade.jar D:\Openfire\openfire_snapshot\plugins\pade.jar
-
-rd "D:\Projects\openfire-cluster\node1\plugins\pade" /q /s
-del "D:\Projects\openfire-cluster\node1\plugins\pade.jar" 
-del /q "D:\Projects\openfire-cluster\node1\logs\*.*"
-copy pade.jar D:\Projects\openfire-cluster\node1\plugins\pade.jar
-
-rd "D:\Projects\openfire-cluster\node2\plugins\pade" /q /s
-del "D:\Projects\openfire-cluster\node2\plugins\pade.jar" 
-del /q "D:\Projects\openfire-cluster\node2\logs\*.*"
-copy pade.jar D:\Projects\openfire-cluster\node2\plugins\pade.jar
-
-rd "D:\Projects\openfire-fmuc\node1\plugins\pade" /q /s
-del "D:\Projects\openfire-fmuc\node1\plugins\pade.jar" 
-del /q "D:\Projects\openfire-fmuc\node1\logs\*.*"
-copy pade.jar D:\Projects\openfire-fmuc\node1\plugins\pade.jar
-
-rd "D:\Projects\openfire-fmuc\node2\plugins\pade" /q /s
-del "D:\Projects\openfire-fmuc\node2\plugins\pade.jar" 
-del /q "D:\Projects\openfire-fmuc\node2\logs\*.*"
-copy pade.jar D:\Projects\openfire-fmuc\node2\plugins\pade.jar
 
 pause
